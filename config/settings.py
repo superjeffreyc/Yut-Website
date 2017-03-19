70eb1fd867e2d839a004f4f8d5cd95a3e09aea9e
Jeffrey Chan <jchan63@binghamton.edu>
2017-03-19 20:01:40 +0000

    Renamed hello and gettingstarted folders

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'src'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import src.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^privacy', hello.views.privacy, name='privacy'),
+    url(r'^$', src.views.index, name='index'),
+    url(r'^privacy', src.views.privacy, name='privacy'),
     #url(r'^admin/', include(admin.site.urls)),
 ]
